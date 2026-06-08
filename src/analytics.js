27b4e683cc5ae03ae0207cf692cf96c5d1ddecf8
Martin-do <martinjaiyeola40@gmail.com>
2026-06-08 17:04:22 +0100

    feat: Add actual GA4 Measurement ID

--- src/analytics.js
+++ src/analytics.js
@@ -1,16 +1,10 @@
 import ReactGA from 'react-ga4';
 
 export const initGA = () => {
-  const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
-  if (measurementId) {
-    ReactGA.initialize(measurementId);
-  } else {
-    console.warn('Google Analytics Measurement ID is missing. Analytics not initialized.');
-  }
+  // Using the actual Measurement ID you provided
+  ReactGA.initialize('G-0J08ZTPRQH');
 };
 
 export const logPageView = () => {
-  if (import.meta.env.VITE_GA_MEASUREMENT_ID) {
-    ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
-  }
+  ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 };
